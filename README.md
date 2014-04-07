507e6e0a764011c1568dc4efda659b4771f37c37
Rizky Ramadhan <rizky@Aryas-MacBook.local>
2014-04-07 17:56:12 +0700

    updating install script

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 NGINX Web Configurator (NWC)
 ============================
 
-**NWC** is web app to ***easily*** configure your NGINX installation. You can also use it to monitor your server health and also install many popular web app on top of your NGINX installation.
+NWC is a web app to configure your nginx installation. You can also use it to monitor your server health and also install many popular web app on top of your NGINX installation.
 
 Screenshoot
 -----------
--- install.php
+++ install.php
@@ -1,3 +1,22 @@
 <?php
 
-echo "FFF";
+$install_dir = "/var/nwc";
+
+
+if (getcwd() != $install_dir) {
+    echo "Installing NWC to {$install_dir}";
+    
+    if (is_dir($install_dir)) {
+        $process = "ps aux | grep /var/nwc | awk 'FNR == 1 {print $2}'";
+        $process = exec($process);
+        exec ("kill {$process}");
+        exec ("rm -rf {$install_dir}");
+    } 
+    
+    mkdir($install_dir);    
+    exec ("cp -R * {$install_dir}");
+    chdir($install_dir);
+} else {
+    echo "Installing from {$install_dir}";
+}
+
