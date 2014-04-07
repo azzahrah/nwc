dd3db28ff2537fcf6f59f914b4b1f5fd2fb56608
Rizky Ramadhan <rizky@Aryas-MacBook.local>
2014-04-07 18:00:12 +0700

    updating installation script

--- install.php
+++ install.php
@@ -2,9 +2,20 @@
 
 $install_dir = "/var/nwc";
 
+echo "
+ ________   ___       __   ________     
+|\   ___  \|\  \     |\  \|\   ____\    
+\ \  \\ \  \ \  \    \ \  \ \  \___|    
+ \ \  \\ \  \ \  \  __\ \  \ \  \       
+  \ \  \\ \  \ \  \|\__\_\  \ \  \____  
+   \ \__\\ \__\ \____________\ \_______\
+    \|__| \|__|\|____________|\|_______|
+
+Welcome to NWC Installation\n\n
+";
 
 if (getcwd() != $install_dir) {
-    echo "Installing NWC to {$install_dir}";
+    echo "Installing NWC to {$install_dir} \n";
     
     if (is_dir($install_dir)) {
         $process = "ps aux | grep /var/nwc | awk 'FNR == 1 {print $2}'";
@@ -17,6 +28,6 @@ if (getcwd() != $install_dir) {
     exec ("cp -R * {$install_dir}");
     chdir($install_dir);
 } else {
-    echo "Installing from {$install_dir}";
+    echo "Installing from {$install_dir} \n";
 }
 
